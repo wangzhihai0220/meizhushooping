8a629f254f859444dbd39b3ce8c3fdb778aef611
wangzhihai0220 <1007195420@qq.com>
2020-09-19 18:35:02 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Hello.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Header
   }
 }
 </script>
@@ -21,8 +22,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+       <img src="src\assets\logo.png" width="140px" height="26px">                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+    
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+.header{
+    height: 80px;
+    width: 100%;
+    background-color: white;
+    border-bottom: 1px solid grey;
+}
+</style>
